d05c0f34b2b44bdc88887ecac67943bfb32c6982
Nora Sandler <gulnora.touraeva@gmail.com>
2021-03-31 00:34:53 -0400

    removing ssl problem

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ module.exports = {
     DB_URL: process.env.DB_URL || 'postgres://xxwzydabjjoglq:72080ccfd7810a63e317a85f68cbac0e791e70082a7de829e274104e7f1e0332@ec2-35-169-92-231.compute-1.amazonaws.com:5432/d6gon6q6prri39',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     apiKey: process.env.apiKey || '',
-    CLIENT_ORIGIN: '*'
+    CLIENT_ORIGIN: '*',
+    PGSSLMODE: "require"
 }
